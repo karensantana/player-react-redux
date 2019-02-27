5b7a48052a5b9539d56ff16f3a733e4432e06435
karensantana <itt.karensantana@gmail.com>
2019-02-27 18:35:27 -0400

    Adding App and SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong} from '../actions'
 
 class SongList extends React.Component {
+    renderList(){
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <buttom className="ui button primary">
+                            Select
+                        </buttom>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        });
+    }
     render(){
-        console.log();
         return (
-            <div> Hello World</div>
+            <div className="ui divided list">{this.renderList()}</div>
         );
     }
 }
 
 const mapStateToProps = (state) => {
- 
    return { songs: state.songs}; 
 }
 
